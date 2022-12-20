893c8fe0d69fcf57aebd3e8ff078fcfa2a9b8272
Msallam2 <94345270+Msallam2@users.noreply.github.com>
2022-12-20 17:46:13 +0100

    + List<Question>

--- src/main/java/de/hebk/Fragen.java
+++ src/main/java/de/hebk/Fragen.java
@@ -1,5 +1,6 @@
 package de.hebk;
 import de.hebk.model.list.List;
+import de.hebk.model.node.Node;
 
 import java.util.Arrays;
 
@@ -8,16 +9,12 @@ public class Fragen {
     private String[] kategorien = {
       "doku/Fragen/Geschichte.csv"
     };
-    private int anzahl = 26;
+    private int anzahl;
     String kategorie;
     private int difficultyValue = 20;
     private int difficultyRange = 0;
-    private double difficultyRate = 0.1;
-
-    private int incrementValue = 0;
-    private int incrementRange = 0;
-    private double incrementRate = 0.05;
-
+    private int incrementValue = 5;
+    private int incrementRange = 5;
 
     Fragen(int a, String k){
         anzahl = a;
@@ -34,38 +31,24 @@ public class Fragen {
             List<String> o2 = csvReader.read_List_String(kategorien[0],3);
             List<String> o3 = csvReader.read_List_String(kategorien[0],4);
             List<String> o4 = csvReader.read_List_String(kategorien[0],5);
-
-            System.out.println(f.toString());
-            String[] possible = new String[600];
-
-            String[] o1 = new String[4];
-
-            List<String[]> f2 = csvReader.read(kategorien[0],0);
-
-            System.out.println(f2.toString() + " f2");
-            System.out.println(f2.get(1).getContext()[2]);
-            f2.toFirst();
-            f2.remove();
-            System.out.println(Arrays.toString(f2.get(0).getContext()));
-
-            List<List<String>> l = new List<>();
-            List<Integer> l2 = new List<>();
-            l.append(f);
-            l.append(d);
-            l.append(r);
-            l.append(o2);
-            l.append(o3);
-            l.append(o4);
+            List<Question> questions = new List<>();
 
             for (int i = 1; i<f.size() + 1; i++){
-                System.out.println(d.get(i).getContext());
                 d.get(i).setContext(d.get(i).getContext().replaceAll("\\D+", "1000"));
                 if (Integer.parseInt(d.get(i).getContext()) <= difficultyValue && Integer.parseInt(d.get(i).getContext()) >= difficultyRange){
-                    l2.append(i);
                     difficultyValue+= incrementValue;
                     difficultyRange+= incrementRange;
-                    difficultyRate+= incrementRate;
-                    //difficultyValue+= difficultyValue*(difficultyRate);
+
+                    Question q = new Question();
+                    q.setQuestion(f.get(i).getContext().replaceAll("<K>", ","));
+                    q.setDifficulty((byte) Integer.parseInt(d.get(i).getContext()));
+                    q.setOptions(new List<>());
+                    q.getOptions().append(r.get(i).getContext());
+                    q.getOptions().append(o2.get(i).getContext());
+                    q.getOptions().append(o3.get(i).getContext());
+                    q.getOptions().append(o4.get(i).getContext());
+                    questions.append(q);
+
                     if (difficultyRange > 60){
                         difficultyRange = 60;
                     }
@@ -74,17 +57,16 @@ public class Fragen {
                         difficultyRange = difficultyValue--;
                     }
 
-                    if (l2.size() >= anzahl){
+                    if (questions.size() >= anzahl){
                         break;
                     }
                 }
             }
-            System.out.println(l.size());
-            System.out.println(l2.toString());
-            System.out.println(l2.size());
-            System.out.println(anzahl);
-            System.out.println(l2.size()/anzahl);
 
+            for (int i = 0; i < questions.size(); i++){
+                //System.out.println(questions.get(i).getContext().getQuestion() + "\n" + questions.get(i).getContext().getOptions().toString());
+                System.out.println(questions.get(i).getContext().getOptions().toString());
+            }
         }
     }
 }
