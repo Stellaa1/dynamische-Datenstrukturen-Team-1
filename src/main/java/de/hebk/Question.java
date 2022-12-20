f71926df29ff805cb27000fc77882fb044fdf59c
Msallam2 <94345270+Msallam2@users.noreply.github.com>
2022-12-20 17:12:09 +0100

    + Class Question.java

---
+++ src/main/java/de/hebk/Question.java
@@ -0,0 +1,33 @@
+package de.hebk;
+
+import de.hebk.model.list.List;
+
+public class Question {
+    private String question;
+    private Byte difficulty;
+    private List<String> options; // Answer = options.get(0)
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public Byte getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Byte difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public List<String> getOptions() {
+        return options;
+    }
+
+    public void setOptions(List<String> options) {
+        this.options = options;
+    }
+}
