9b8d8ac28aaf838f487c487e11a4198f1783fb5d
Msallam2 <msallam.al-jwabreh@hebk.de>
2022-12-22 11:34:40 +0100

    + Generate Questions for every csv File in doku/Fragen

--- src/main/java/de/hebk/Fragen.java
+++ src/main/java/de/hebk/Fragen.java
@@ -15,23 +15,30 @@ public class Fragen {
     private int difficultyRange = 0;
     private int incrementValue = 5;
     private int incrementRange = 5;
+    private List<Question> questions = new List<>();
+
 
     Fragen(int a, String k){
         anzahl = a;
         kategorie = k;
     }
 
+    public int getAnzahl(){
+        return anzahl;
+    }
+    public List<Question> getQuestions(){
+        return questions;
+    }
+
     public void generateQuestions(){
 
-        if (kategorie.equals("Geschichte")){
             CSVReader csvReader = new CSVReader();
-            List<String> f = csvReader.read_List_String(kategorien[0],0);
-            List<String> d = csvReader.read_List_String(kategorien[0],1);
-            List<String> r = csvReader.read_List_String(kategorien[0],2);
-            List<String> o2 = csvReader.read_List_String(kategorien[0],3);
-            List<String> o3 = csvReader.read_List_String(kategorien[0],4);
-            List<String> o4 = csvReader.read_List_String(kategorien[0],5);
-            List<Question> questions = new List<>();
+            List<String> f = csvReader.read_List_String("doku/Fragen/" + kategorie + ".csv",0);
+            List<String> d = csvReader.read_List_String("doku/Fragen/" + kategorie + ".csv",1);
+            List<String> r = csvReader.read_List_String("doku/Fragen/" + kategorie + ".csv",2);
+            List<String> o2 = csvReader.read_List_String("doku/Fragen/" + kategorie + ".csv",3);
+            List<String> o3 = csvReader.read_List_String("doku/Fragen/" + kategorie + ".csv",4);
+            List<String> o4 = csvReader.read_List_String("doku/Fragen/" + kategorie + ".csv",5);
 
             for (int i = 1; i<f.size() + 1; i++){
                 d.get(i).setContext(d.get(i).getContext().replaceAll("\\D+", "1000"));
@@ -68,5 +75,5 @@ public class Fragen {
                 System.out.println(questions.get(i).getContext().getOptions().toString());
             }
         }
-    }
+
 }
