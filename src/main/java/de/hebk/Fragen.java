1c97005f9421e6f50d0f8d6e933ce7cb70cfe1a9
Msallam2 <msallam.al-jwabreh@hebk.de>
2022-12-23 19:54:12 +0100

    → Apply GameSettings

--- src/main/java/de/hebk/Fragen.java
+++ src/main/java/de/hebk/Fragen.java
@@ -5,66 +5,57 @@ import de.hebk.model.node.Node;
 import java.util.Arrays;
 
 public class Fragen {
-
-    private String[] kategorien = {
-      "doku/Fragen/Geschichte.csv"
-    };
-    private int anzahl;
-    String kategorie;
-    private int difficultyValue = 20;
-    private int difficultyRange = 0;
-    private int incrementValue = 5;
-    private int incrementRange = 5;
     private List<Question> questions = new List<>();
 
+    GameSettings gameSettings = new GameSettings();
 
-    Fragen(int a, String k){
-        anzahl = a;
-        kategorie = k;
-    }
-
-    public int getAnzahl(){
-        return anzahl;
-    }
     public List<Question> getQuestions(){
         return questions;
     }
 
     public void generateQuestions(){
-
             CSVReader csvReader = new CSVReader();
-            List<String> f = csvReader.read_List_String("doku/Fragen/" + kategorie + ".csv",0);
-            List<String> d = csvReader.read_List_String("doku/Fragen/" + kategorie + ".csv",1);
-            List<String> r = csvReader.read_List_String("doku/Fragen/" + kategorie + ".csv",2);
-            List<String> o2 = csvReader.read_List_String("doku/Fragen/" + kategorie + ".csv",3);
-            List<String> o3 = csvReader.read_List_String("doku/Fragen/" + kategorie + ".csv",4);
-            List<String> o4 = csvReader.read_List_String("doku/Fragen/" + kategorie + ".csv",5);
+            List<String> f = csvReader.read_List_String("doku/Fragen/" + gameSettings.getCategory() + ".csv",0);
+            List<String> d = csvReader.read_List_String("doku/Fragen/" + gameSettings.getCategory() + ".csv",1);
+            List<String> r = csvReader.read_List_String("doku/Fragen/" + gameSettings.getCategory() + ".csv",2);
+            List<String> o2 = csvReader.read_List_String("doku/Fragen/" + gameSettings.getCategory() + ".csv",3);
+            List<String> o3 = csvReader.read_List_String("doku/Fragen/" + gameSettings.getCategory() + ".csv",4);
+            List<String> o4 = csvReader.read_List_String("doku/Fragen/" + gameSettings.getCategory() + ".csv",5);
+
 
             for (int i = 1; i<f.size() + 1; i++){
                 d.get(i).setContext(d.get(i).getContext().replaceAll("\\D+", "1000"));
-                if (Integer.parseInt(d.get(i).getContext()) <= difficultyValue && Integer.parseInt(d.get(i).getContext()) >= difficultyRange){
-                    difficultyValue+= incrementValue;
-                    difficultyRange+= incrementRange;
+                if (Integer.parseInt(d.get(i).getContext()) <= gameSettings.getDifficultyValue() && Integer.parseInt(d.get(i).getContext()) >= gameSettings.getDifficultyRange()){
+                    gameSettings.setIncrementValue(gameSettings.getDifficultyValue() + gameSettings.getIncrementValue());
+                    gameSettings.setIncrementRange(gameSettings.getDifficultyRange() + gameSettings.getIncrementRange());
 
                     Question q = new Question();
                     q.setQuestion(f.get(i).getContext().replaceAll("<K>", ","));
                     q.setDifficulty((byte) Integer.parseInt(d.get(i).getContext()));
                     q.setOptions(new List<>());
+
+
                     q.getOptions().append(r.get(i).getContext());
                     q.getOptions().append(o2.get(i).getContext());
                     q.getOptions().append(o3.get(i).getContext());
                     q.getOptions().append(o4.get(i).getContext());
                     questions.append(q);
 
-                    if (difficultyRange > 60){
-                        difficultyRange = 60;
+                    int x = (int) (Math.random() * 100);
+                    if (x <= 7){
+                        int z = (int) (Math.random() * 3);
+                        q.getOptions().get(z).setContext("Keine von genannten");
                     }
 
-                    if (difficultyRange >= difficultyValue){
-                        difficultyRange = difficultyValue--;
+                    //if (gameSettings.getDifficultyRange() > 60){
+                        //gameSettings.setDifficultyRange(60);
+                    //}
+
+                    if (gameSettings.getDifficultyRange() >= gameSettings.getDifficultyValue()){
+                        gameSettings.setDifficultyRange(gameSettings.getDifficultyValue() - 1);
                     }
 
-                    if (questions.size() >= anzahl){
+                    if (questions.size() >= gameSettings.getQuestion_Amount()){
                         break;
                     }
                 }
