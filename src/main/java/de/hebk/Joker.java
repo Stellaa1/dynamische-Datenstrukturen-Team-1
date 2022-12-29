3a422603460470d1b60ae445d559ecad572d615a
Msallam2 <94345270+Msallam2@users.noreply.github.com>
2022-12-29 15:17:05 +0100

    + New Joker class → boolean infJoker; → boolean revive; → boolean audience; → boolean fifty_fifty; → boolean call; + getter() and setter() for every attribute

---
+++ src/main/java/de/hebk/Joker.java
@@ -0,0 +1,49 @@
+package de.hebk;
+
+public class Joker {
+    private boolean infJoker;
+    private boolean revive;
+    private boolean audience;
+    private boolean fifty_fifty;
+    private boolean call;
+
+    public boolean isInfJoker() {
+        return infJoker;
+    }
+
+    public void setInfJoker(boolean infJoker) {
+        this.infJoker = infJoker;
+    }
+
+    public boolean isRevive() {
+        return revive;
+    }
+
+    public void setRevive(boolean revive) {
+        this.revive = revive;
+    }
+
+    public boolean isAudience() {
+        return audience;
+    }
+
+    public void setAudience(boolean audience) {
+        this.audience = audience;
+    }
+
+    public boolean isFifty_fifty() {
+        return fifty_fifty;
+    }
+
+    public void setFifty_fifty(boolean fifty_fifty) {
+        this.fifty_fifty = fifty_fifty;
+    }
+
+    public boolean isCall() {
+        return call;
+    }
+
+    public void setCall(boolean call) {
+        this.call = call;
+    }
+}
